c9699cebc1a273d580cd7f4c93008d5a12400ad9
ariktwena <ariktwena@gmail.com>
2020-07-22 13:21:24 +0200

    Updates

---
+++ precache-manifest.a4425d39f99c7e179a36829fc8f543b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94821ff65ded1e3616318713b0c4ec2a",
+    "url": "/github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1cc54f3016f32fb479c3",
+    "url": "/github.io/monster-rolodex/static/css/main.fafd675d.chunk.css"
+  },
+  {
+    "revision": "59e07d13fedd4a313f37",
+    "url": "/github.io/monster-rolodex/static/js/2.4bb0ac66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monster-rolodex/static/js/2.4bb0ac66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc54f3016f32fb479c3",
+    "url": "/github.io/monster-rolodex/static/js/main.42dce668.chunk.js"
+  },
+  {
+    "revision": "7f05c795fca003d60b04",
+    "url": "/github.io/monster-rolodex/static/js/runtime-main.a13a4927.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
